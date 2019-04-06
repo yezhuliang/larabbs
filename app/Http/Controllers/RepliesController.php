fcfa9305552f5832ee4bfeccf3f36fe985f57ae5
yezhu <1501905755@qq.com>
2019-04-06 15:09:20 +0000

    Notify author when topic replied

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -17,16 +17,18 @@ class RepliesController extends Controller
 
 	public function store(ReplyRequest $request, Reply $reply)
     {
-        if (empty($content)) {
-            return redirect()->back()->with('danger', '回复内容错误！');
-        }
 
         $reply->content = $request->content;
         $reply->user_id = Auth::id();
         $reply->topic_id = $request->topic_id;
         $reply->save();
 
-        return redirect()->to($reply->topic->link())->with('success', '创建成功！');
+        if (empty($reply->content)) {
+            return redirect()->back()->with('danger', '回复内容错误！');
+        } else {
+            return redirect()->to($reply->topic->link())->with('success', '创建成功！');
+        }
+
     }
 
     public function destroy(Reply $reply)
